7ec18554ab5e3b770b77cb40fa7f1f47759e3179
RoyCuthbert <roycuthbert@hotmail.com>
2026-05-05 16:51:18 +0100

    Added category in menu and linked to database

--- menu/models.py
+++ menu/models.py
@@ -4,8 +4,15 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class MenuItem(models.Model):
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='menu/', default='menu/default.jpg')
+
+    def __str__(self):
+        return self.name
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import MenuItem, Category
 
 # Create your views here.
 def menu(request):
-    return HttpResponse("TESTER")
+    categories = Category.objects.all()
+    items = MenuItem.objects.all()
+    return render(request, 'menu/menu.html', {'categories':categories, 'items':items})
