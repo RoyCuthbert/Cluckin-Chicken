dcc7dff567e46b53aa804395c00efd2c08ada3d7
RoyCuthbert <roycuthbert@hotmail.com>
2026-05-05 15:39:02 +0100

    Added menu model and created database in admin

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class MenuItem(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
