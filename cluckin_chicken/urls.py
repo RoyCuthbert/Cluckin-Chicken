390d9bbb36a88d30d4cb06ee6802982bbcd5f074
RoyCuthbert <roycuthbert@hotmail.com>
2026-05-11 15:56:59 +0100

    added contact page and update cart link on navbar also a cleanup on files

--- cluckin_chicken/urls.py
+++ cluckin_chicken/urls.py
@@ -19,14 +19,14 @@ from django.urls import path, include
 from core import views as core_view
 from menu import views as menu_view
 from booking import views as booking_view
-from order import views as order_view
+from core import views as contact_view
 from accounts import views as accounts_view
 
 urlpatterns = [
     path('', core_view.home, name='core'),
     path('menu/', menu_view.menu, name = 'menu'),
     path('booking/', booking_view.booking_view, name = 'booking'),
-    path('order/', order_view.order, name = 'order'),
+    path('contact/', contact_view.contact, name = 'contact'),
     path('accounts/', accounts_view.accounts, name='accounts'),
     path('admin/', admin.site.urls),
 ]
