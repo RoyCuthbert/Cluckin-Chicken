5f3e3c43bbf98b668d0967399ee27ab54dc631c4
RoyCuthbert <roycuthbert@hotmail.com>
2026-05-19 12:21:08 +0100

    updating project urls

--- cluckin_chicken/urls.py
+++ cluckin_chicken/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core import views as core_view
-from menu import views as menu_view
-from booking import views as booking_view
 from core import views as contact_view
 from accounts import views as accounts_view
 from django.conf.urls.static import static
@@ -27,8 +25,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('', core_view.home, name='home'),
-    path('menu/', menu_view.menu, name = 'menu'),
-    path('booking/', booking_view.booking_view, name = 'booking'),
+    path('menu/', include('menu.urls')),
+    path('booking/', include('booking.urls')),
     path('contact/', contact_view.contact, name = 'contact'),
     path('accounts/', accounts_view.accounts, name='accounts'),
     path('cart/', include('order.urls')),
