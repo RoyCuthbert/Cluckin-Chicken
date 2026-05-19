449f4792216ecbc1bc235ce4cca932240d9b23a9
RoyCuthbert <roycuthbert@hotmail.com>
2026-05-19 12:48:26 +0100

    updating booking views

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', booking_view, name='booking'),
+    path('', views.booking_view, name='booking'),
     path('success', views.booking_success, name='booking_success')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Booking
+from django.contrib import messages
 
 # Create your views here.
 def booking_view(request):
     if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        date = request.POST.get('date')
+        time = request.POST.get('time')
+        guests = request.POST.get('guests')
+        
+        if not name or not email:
+            return render(request, 'booking/booking.html')
+        
         Booking.objects.create(
-            date = request.POST.get('date'),
-            time = request.POST.get('time'),
-            guests = request.POST.get('guests')
+            name = name,
+            email = email,
+            date = date,
+            time = time,
+            guests = guests,
         )
+        messages.success(request, "Booking Successful!")
+        return redirect('booking')
         
         return redirect('booking')
 
