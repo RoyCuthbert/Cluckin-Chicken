c66b2015cf9815d8f8c878b5561583e048e21ad4
RoyCuthbert <roycuthbert@hotmail.com>
2026-05-19 12:12:25 +0100

    Adding further fields into booking models

--- booking/models.py
+++ booking/models.py
@@ -4,11 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Booking(models.Model):
-    User = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(default=User, max_length=100)
+    email = models.EmailField(default=User)
     date = models.DateField()
     time = models.TimeField()
     guests = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.date} {self.time}"
+        return f"{self.name} {self.time}"
