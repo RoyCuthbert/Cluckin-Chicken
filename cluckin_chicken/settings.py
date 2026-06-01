a98c2e08b731acdd6e9d2022984f7393f3dbe13c
RoyCuthbert <roycuthbert@hotmail.com>
2026-06-01 21:59:40 +0100

    changing allowed hosts

--- cluckin_chicken/settings.py
+++ cluckin_chicken/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','*.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','https://cluckin-chicken-3cf5c5195fcf.herokuapp.com/']
 
 
 # Application definition
