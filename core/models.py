2b59607e47c2bf54a59083ca98922932c5b3605d
RoyCuthbert <roycuthbert@hotmail.com>
2026-06-07 20:17:26 +0100

    Adding a message sent in the contact page

--- core/models.py
+++ core/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class ContactMessage(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    email = models.EmailField()
+
+    message = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return self.name
--- core/views.py
+++ core/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from menu.models import MenuItem
+from .models import ContactMessage
 
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
 
 def contact(request):
+    if request.method == 'POST':
+
+        ContactMessage.objects.create(
+
+            name=request.POST.get('name'),
+            email=request.POST.get('email'),
+            message=request.POST.get('message')
+
+        )
+
+        messages.success(
+            request,
+            'Your message has been sent successfully!'
+        )
+
+        return redirect('contact')
+
     return render(request, 'core/contact.html')
+
